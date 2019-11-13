99959c82b3de0edf9d5d4f2bf65c0dc03ea8b352
Stephanie <sbowl001@fiu.edu>
2019-11-14 01:07:00 +0800

    calculator

--- Sprint 11/Module 4/RPNCalculator OBjc/RPNCalculator OBjc/LSICalculator.h
+++ Sprint 11/Module 4/RPNCalculator OBjc/RPNCalculator OBjc/LSICalculator.h
@@ -10,8 +10,22 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
+typedef NS_ENUM(NSInteger, LSICalculatorOperator) {
+    LSICalculatorOperatorAdd,
+    LSICalculatorOperatorSubtract,
+    LSICalculatorOperatorMultiply,
+    LSICalculatorOperatorDivide
+};
+
 @interface LSICalculator : NSObject
 
+-(void)pushNumber:(double)value;
+- (void)applyOperator:(LSICalculatorOperator)operator;
+- (void)clear;
+
+@property (readonly) double topValue; //not on instructions
 @end
 
 NS_ASSUME_NONNULL_END
+
+//.h file: - Create an enum for the operations that will be calculated using typedef NS_ENUM(rawType, nameOfEnum) {};. Inside the curly braces you will just need to separate each item with a comma. - Declare three methods - (void)pushNumber:(double)value;, - (void)applyOperator:(YourEnumType)operator; and - (void)clear;. - Also declare a topValue property that will be a computed property in your .m file.
--- Sprint 11/Module 4/RPNCalculator OBjc/RPNCalculator OBjc/LSICalculator.m
+++ Sprint 11/Module 4/RPNCalculator OBjc/RPNCalculator OBjc/LSICalculator.m
@@ -7,7 +7,58 @@
 //
 
 #import "LSICalculator.h"
+#import "LSIStack.h"
+@interface LSICalculator()
 
+@property (nonatomic, strong) LSIStack *stack;
+@end
 @implementation LSICalculator
 
+-(instancetype)init
+{
+    self = [super init];
+    if (self) {
+        _stack = [[LSIStack alloc] init];
+    }
+    return self;
+}
+
+-(void)pushNumber:(double)value
+{
+    [self.stack pushValue:value];
+}
+- (void)applyOperator:(LSICalculatorOperator)operator
+{
+    double operand2 = [self.stack popLastValue];
+    double operand1 = [self.stack popLastValue];
+    
+    double result = 0.0;
+    switch (operator) {
+        case LSICalculatorOperatorAdd:
+            result = operand1 + operand2;
+            break;
+        case LSICalculatorOperatorSubtract:
+            result = operand1 - operand2;
+            break;
+        case LSICalculatorOperatorMultiply:
+            result = operand1 * operand2;
+            break;
+        case LSICalculatorOperatorDivide:
+            result = operand1 / operand2;
+            break;
+    }
+    
+    [self pushNumber:result];
+}
+- (void)clear
+{
+    self.stack = [[LSIStack alloc] init];
+}
+
+-(double)topValue
+{
+    return [self.stack peekLastValue];
+}
+
 @end
+//.m file: - Every Calculator should have one Stack and only that calculator should know about it's Stack. Create a property XXXStack *stack in this file that we will use to implement the methods. - Implement the methods in this file by accessing the methods on your Stack model.
